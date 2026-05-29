58483a8f4641532237c67c0c1b7666f800c7fad6
anshika4515 <anshikaverma4515@gmail.com>
2026-05-29 12:28:43 +0530

    Bundle node_modules with custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,18 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
 
 async function run() {
+    //1 Get some input
+   const bucket = core.getInput('bucket', { required : true});
+   const bucketRegion = core.getInput('bucket-region', { required : false});
+   const distFolder = core.getInput('dist-folder', { required : false});
+
+
+   //2. Upload files
+   const s3Uri = `s3://${bucket}`
+   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
    core.notice('Hello from custom js msg')
 }
 
